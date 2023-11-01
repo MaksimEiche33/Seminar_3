3174cb60b1776217b5d6b01af9022462c18dea34
MaksimEiche33 <zederundbirke@gmail.com>
2023-11-01 13:18:39 +0300

    Section 8. Spinner Drop-down List

--- app/src/main/java/mai/team2/seminar_23_10_23/MainActivity.java
+++ app/src/main/java/mai/team2/seminar_23_10_23/MainActivity.java
@@ -1,85 +1,44 @@
 package mai.team2.seminar_23_10_23;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
-/*
 public class MainActivity extends AppCompatActivity {
-    */
-/*//*
-/ набор данных, которые свяжем со списком
-    String[] countries = { "Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};*//*
 
-    ArrayList<State> states = new ArrayList<State>();
-    ListView countriesList;
+    String[] countries = { "Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // начальная инициализация списка
-        setInitialData();
-        // получаем элемент ListView
-        countriesList = findViewById(R.id.countriesList);
-        // создаем адаптер
-        StateAdapter stateAdapter = new StateAdapter(this, R.layout.list_item, states);
-        // устанавливаем адаптер
-        countriesList.setAdapter(stateAdapter);
-        // слушатель выбора в списке
-        AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 
-                // получаем выбранный пункт
-                State selectedState = (State)parent.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(), "Был выбран пункт " + selectedState.getName(),
-                        Toast.LENGTH_SHORT).show();
+        TextView selection = findViewById(R.id.selection);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, countries);
+        // Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Применяем адаптер к элементу spinner
+        spinner.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                // Получаем выбранный объект
+                String item = (String)parent.getItemAtPosition(position);
+                selection.setText(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
             }
         };
-        countriesList.setOnItemClickListener(itemListener);
-    }
-    private void setInitialData(){
-
-        states.add(new State ("Бразилия", "Бразилиа", R.drawable.brazil));
-        states.add(new State ("Аргентина", "Буэнос-Айрес", R.drawable.argent));
-        states.add(new State ("Колумбия", "Богота", R.drawable.columb));
-        states.add(new State ("Уругвай", "Монтевидео", R.drawable.urug));
-        states.add(new State ("Чили", "Сантьяго", R.drawable.chili));
-    }
-}*/
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.ListView;
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ArrayList<Product> products = new ArrayList<Product>();
-        if(products.size()==0){
-            products.add(new Product("Картофель", "кг."));
-            products.add(new Product("Чай", "шт."));
-            products.add(new Product("Яйца", "шт."));
-            products.add(new Product("Молоко", "л."));
-            products.add(new Product("Макароны", "кг."));
-        }
-        ListView productList = findViewById(R.id.productList);
-        ProductAdapter adapter = new ProductAdapter(this, R.layout.list_item, products);
-        productList.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(itemSelectedListener);
     }
 }
